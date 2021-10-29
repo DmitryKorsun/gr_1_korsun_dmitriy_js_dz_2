d82175da667f961fde383ba9e9696db8258f5ee5
Dmytro Korsun <79083914+DmitryKorsun@users.noreply.github.com>
2021-10-29 12:00:28 +0300

    Add files via upload

---
+++ JS/homework_1.js
@@ -0,0 +1,14 @@
+let name = 'Дмитрий', lastName = 'Корсун', surName = 'Владиславович';
+let colorHair = 'Черный';
+let heigt = 166, weigth = 49;
+const сolorEye = 'Серо-зеленый';
+const dateBirth = '08.01.2001';
+const numberLegs = 2, numberHands = 2;
+alert(`Я, ${name} ${lastName} ${surName}. Родился: ${dateBirth}
+Характеристика: 
+Имею количество ног: ${numberLegs}. 
+Количество рук: ${numberHands}. 
+Цвет волос: ${colorHair}. 
+Цвет глаз: ${сolorEye}
+Рост: ${heigt} cм
+Вес: ${weigth} кг`);
