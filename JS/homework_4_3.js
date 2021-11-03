9bbd7f0ce155226dbefd53830f02b9ef9641f859
Dmytro Korsun <79083914+DmitryKorsun@users.noreply.github.com>
2021-11-03 12:05:56 +0200

    Add files via upload

---
+++ JS/homework_4_3.js
@@ -0,0 +1,7 @@
+let string = prompt('Введите слова которое хотите переобразовать',0);
+
+reverseString (string);
+
+function reverseString() {
+    return alert(string.split('').reverse().join(''));
+}
