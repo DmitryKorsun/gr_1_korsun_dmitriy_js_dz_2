b155528f8847fdc6dca4e8c0f2b6035e71d1ff4e
Dmytro Korsun <79083914+DmitryKorsun@users.noreply.github.com>
2021-11-03 13:18:38 +0200

    Delete homework_4_3.js

--- JS/homework_4_3.js
+++
@@ -1,7 +0,0 @@
-let string = prompt('Введите слова которое хотите переобразовать',0);
-
-reverseString (string);
-
-function reverseString() {
-    return alert(string.split('').reverse().join(''));
-}
